c00e448a361da26a89d4b425ee944e2d0f5ba93a
stupidblackmonkey <greenherobrin1979@tutamail.com>
2025-11-27 16:04:40 +0300

    Add files via upload

--- papa.py
+++ papa.py
@@ -2,7 +2,11 @@ import Is_even
 import circle
 import temperature
 import sum
-Is_even.main()
-circle.main()
-sum.main()
-temperature.main()
+def main():
+    Is_even.main()
+    circle.main()
+    sum.main()
+    temperature.main()
+
+if __name__ == '__main__':
+    main()
